550e6f7b585d4673d243888aa6594db714b3efab
Vitaliy Vrublevskiy <vitaliyvrublevskiy@gmail.com>
2016-10-22 18:24:33 +0300

    add callback for takePictureButton

--- src/main/java/com/example/final_camera2/CameraHelper.java
+++ src/main/java/com/example/final_camera2/CameraHelper.java
@@ -1,9 +1,12 @@
 package com.example.final_camera2;
 
+import android.util.Log;
+
 public class CameraHelper {
+    private static final String TAG = "Camera Helper";
 
     public void getImage() {
-
+        Log.e(TAG, "In getImage()");
     }
 
 }
--- src/main/java/com/example/final_camera2/MainActivity.java
+++ src/main/java/com/example/final_camera2/MainActivity.java
@@ -2,16 +2,27 @@ package com.example.final_camera2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private CameraHelper cameraHelper;
     private Button takePictureButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         takePictureButton = (Button) findViewById(R.id.btn_get_photo);
         assert takePictureButton != null;
+
+        cameraHelper = new CameraHelper();
+
+        takePictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cameraHelper.getImage();
+            }
+        });
     }
 }
