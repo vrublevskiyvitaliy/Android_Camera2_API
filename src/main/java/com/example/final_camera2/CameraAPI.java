e6093a777b27423666bfa0b3adeb5a26966304b8
Vitaliy Vrublevskiy <vitaliyvrublevskiy@gmail.com>
2016-10-22 18:19:21 +0300

    add camera helper and cameraAPI

---
+++ src/main/java/com/example/final_camera2/CameraAPI.java
@@ -0,0 +1,5 @@
+package com.example.final_camera2;
+
+public class CameraAPI {
+
+}
---
+++ src/main/java/com/example/final_camera2/CameraHelper.java
@@ -0,0 +1,9 @@
+package com.example.final_camera2;
+
+public class CameraHelper {
+
+    public void getImage() {
+
+    }
+
+}
